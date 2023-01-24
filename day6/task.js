dd7d73a8e2c9a40345fe28111aedfa10dc769a24
Vladimir Novichikhin <vladimir@oken.tech>
2023-01-25 02:39:14 +0300

    day6 fixed version

--- day6/task.js
+++ day6/task.js
@@ -3,20 +3,14 @@ const path = require("path");
 
 const input = getInput('input');
 
-let checkUniqueObj = {};
-let count = 0;
+const SUBSTRING_LENGTH = 4;
+
 let result;
 
 for (let i = 0; i < input.length; i++) {
-    if (!checkUniqueObj[input[i]]) {
-        checkUniqueObj[input[i]] = true;
-        count++;
-    } else {
-        checkUniqueObj = {};
-        count = 0;
-    }
-    if (count === 4) {
-        result = i;
+    const substring = input.slice(i, i + SUBSTRING_LENGTH);
+    if (new Set(substring).size === SUBSTRING_LENGTH) {
+        result = i + SUBSTRING_LENGTH;
         break;
     }
 }
